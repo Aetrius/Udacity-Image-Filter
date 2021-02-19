875a9a41b885e2444eaea323578e7bf439172275
Tyler B <tylerwbennet@gmail.com>
2021-02-19 00:05:10 -0600

    added endpoint to API

--- server.ts
+++ server.ts
@@ -12,7 +12,7 @@ import {filterImageFromURL, deleteLocalFiles} from './src/util/util';
   
   // Use the body parser middleware for post requests
   app.use(bodyParser.json());
-
+  var validUrl = require('valid-url');
   // @TODO1 IMPLEMENT A RESTFUL ENDPOINT
   // GET /filteredimage?image_url={{URL}}
   // endpoint to filter an image from a public url.
@@ -28,8 +28,67 @@ import {filterImageFromURL, deleteLocalFiles} from './src/util/util';
   //   the filtered image file [!!TIP res.sendFile(filteredpath); might be useful]
 
   /**************************************************************************** */
+  app.get( "/filteredimage", async ( req, res ) => {
 
-  //! END @TODO1
+    const image_url:any = req.query.image_url;
+    console.log('output ' + image_url /*+ image_url3 + image_url2*/)
+
+    if (image_url == 'undefined') {
+      return res.status(400)
+    }
+
+    if (!image_url) {
+      return res.status(400).send({ message: 'Image URL is required.' + image_url});
+   }
+
+   var files: string[];
+   var link : string;
+   var path = require('path');
+   const fs = require('fs');
+
+   if (validUrl.isUri(image_url)){
+     await filterImageFromURL(image_url)
+
+     .then(localImageDownload => {
+       res.sendFile(localImageDownload)
+       try {
+        console.log(localImageDownload);
+        link = new URL(localImageDownload).toString();
+        files = [link];
+       } catch (error) {
+         console.log(error)
+       }
+
+      })
+      .then(() => {
+        try {
+          console.log(path.normalize(new URL(link).toString()));
+          var testvar :string;
+          testvar = path.normalize(link).toString();
+          path = testvar.substring(0,testvar.lastIndexOf("\\")+1);
+          console.log("\nFiles present in directory:"); 
+          var dirname = testvar.match(/(.*)[\/\\]/)[1]||'';
+          fs.readdirSync(dirname).forEach((file: any) => {
+            //console.log('list of files: ' + file);
+
+            fs.unlink(path + '\\'+ file,function(err: any){  
+              //console.log(path + '\\'+ file );
+
+            });
+          });
+        } catch {
+          console.log('An error occurred while parsing url');
+        }
+        
+        
+    });  
+   } 
+   else {
+      return res.status(404).send('Unable to locate image at path');
+   }
+
+  });
+
   
   // Root Endpoint
   // Displays a simple message to the user
